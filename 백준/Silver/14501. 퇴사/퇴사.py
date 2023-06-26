1e0af5d63ea3b88fe4d419a0923457159656efcd
titanium9288 <87443489+titanium9288@users.noreply.github.com>
2023-06-26 19:10:55 +0900

    [Silver III] Title: 퇴사, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -0,0 +1,24 @@
+# 테스트 케이스의 갯수
+N = int(input())
+
+# [걸리는 시간, 수익]
+cases = [0]
+for _ in range(N):
+    cases.append(list(map(int, input().split())))
+
+incomes_list = [0 for _ in range(N + 2)]
+
+for i in range(1, N + 1):
+    # 기본적으로 전날의 수익과 같음
+    incomes_list[i] = max(incomes_list[i], incomes_list[i - 1])
+    period = cases[i][0]
+    pay = cases[i][1]
+
+    # 해당 일을 할 수 없는 경우
+    if i + period > N + 1:
+        continue
+
+    # 해당 일을 할 수 있는 경우
+    incomes_list[i + period] = max(incomes_list[i + period], incomes_list[i] + pay)
+
+print(max(incomes_list))
