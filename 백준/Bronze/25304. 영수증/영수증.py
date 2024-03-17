366e1053ce223d67254a5ac09120b483b86ac912
titanium9288 <87443489+titanium9288@users.noreply.github.com>
2024-03-17 22:36:02 +0900

    [Bronze IV] Title: 영수증, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.py
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -1,12 +1,11 @@
 total_price = int(input())
 n = int(input())
-calc_price = 0
 
 for i in range(n):
     price, count = map(int, input().split())
-    calc_price += price * count
+    total_price -= price * count
 
-if calc_price == total_price:
-    print("Yes")
-else:
+if total_price:
     print("No")
+else:
+    print("Yes")
