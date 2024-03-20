ef3f8093ba9cadc2744afc2dd705978c9154d42b
titanium9288 <87443489+titanium9288@users.noreply.github.com>
2024-03-20 11:48:07 +0900

    [Silver I] Title: 가장 가까운 세 사람의 심리적 거리, Time: 708 ms, Memory: 38968 KB -BaekjoonHub

---
+++ 백준/Silver/20529. 가장 가까운 세 사람의 심리적 거리/가장 가까운 세 사람의 심리적 거리.py
@@ -0,0 +1,35 @@
+import sys
+import itertools
+
+
+def calc_distance(s1, s2):
+    return sum(ch1 != ch2 for ch1, ch2 in zip(s1, s2))
+
+
+def make_distance_pair():
+    traits = [["I", "E"], ["S", "N"], ["T", "F"], ["J", "P"]]
+    mbti_types = ["".join(p) for p in itertools.product(*traits)]
+    distances = {}
+    for trio in itertools.combinations_with_replacement(mbti_types, 3):
+        trio_distance = sum(
+            calc_distance(*pair) for pair in itertools.combinations(trio, 2)
+        )
+        distances[tuple(sorted(trio))] = trio_distance
+    return distances
+
+
+distances = make_distance_pair()
+
+for _ in range(int(input())):
+    N = int(input())
+    students = list(input().split())
+
+    # 각 유형의 수를 최대 3으로 제한
+    student_dict = {mbti: min(students.count(mbti), 3) for mbti in set(students)}
+    students = [mbti for mbti, count in student_dict.items() for _ in range(count)]
+
+    min_distance = float("inf")
+    for trio in itertools.combinations(students, 3):
+        min_distance = min(min_distance, distances[tuple(sorted(trio))])
+
+    print(min_distance)
