4f274457a40e3f63beeaffb545cb5cab74372c84
titanium9288 <87443489+titanium9288@users.noreply.github.com>
2025-03-18 15:07:22 +0900

    [Silver II] Title: 유기농 배추, Time: 228 ms, Memory: 34984 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,30 +1,30 @@
 from collections import deque
-import sys
 
-sys.setrecursionlimit(10**6)
+T = int(input())
 
 
-def dfs(x, y):
-    if visited[x][y] or field[x][y] == 0:
-        return 0
-
-    # 방문 처리
+def bfs(x, y):
+    queue = deque([(x, y)])
     visited[x][y] = True
     size = 1
 
-    # 상하좌우
-    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    while queue:
+        x, y = queue.popleft()
 
-    for dx, dy in directions:
-        nx, ny = x + dx, y + dy
+        # 상하좌우
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
-        if 0 <= nx < M and 0 <= ny < N:
-            size += dfs(nx, ny)
+        for dx, dy in directions:
+            nx, ny = x + dx, y + dy
+
+            if 0 <= nx < M and 0 <= ny < N:
+                if not visited[nx][ny] and field[nx][ny]:
+                    visited[nx][ny] = True
+                    queue.append((nx, ny))
+                    size += 1
 
     return size
 
-
-T = int(input())
 
 for i in range(T):
     M, N, K = map(int, input().split())
@@ -43,6 +43,6 @@ for i in range(T):
     for i in range(M):
         for j in range(N):
             if not visited[i][j] and field[i][j] == 1:
-                cluster_count += dfs(i, j) > 0
+                cluster_count += bfs(i, j) > 0
 
     print(cluster_count)
