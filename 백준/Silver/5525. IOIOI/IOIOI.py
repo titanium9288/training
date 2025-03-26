d49b5a0bd7cc1056b7a2adeaa2fd4f4a5500807a
titanium9288 <87443489+titanium9288@users.noreply.github.com>
2025-03-26 12:28:43 +0900

    [Silver I] Title: IOIOI, Time: 268 ms, Memory: 35344 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,10 +1,24 @@
-
 N = int(input())
-P = 'I' + ('OI' * N)
-
 M = int(input())
+
+P_n = "I" + ("OI" * N)
+
 S = input()
+answer = 0
+count = 0
 
-answer = [n for n in range(len(S)) if S.find(P, n) == n]
+i = 1
 
-print(len(answer))
+while i < M - 1:
+    if S[i - 1] == "I" and S[i] == "O" and S[i + 1] == "I":
+        count += 1
+        if N <= count:
+            answer += 1
+        i += 2
+
+    else:
+        count = 0
+        i += 1
+
+
+print(answer)
