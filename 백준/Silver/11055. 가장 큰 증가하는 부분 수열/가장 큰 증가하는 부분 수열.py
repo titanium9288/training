19dd3a4f1cb901cdc2c8bb8105a1740e76a7269b
titanium9288 <87443489+titanium9288@users.noreply.github.com>
2025-04-02 14:06:07 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 144 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.py
@@ -0,0 +1,11 @@
+N = int(input())
+A = list(map(int, input().split()))
+
+dp = A[:]
+
+for i in range(N):
+    for j in range(i):
+        if A[j] < A[i]:
+            dp[i] = max(dp[i], dp[j] + A[i])
+
+print(max(dp))
