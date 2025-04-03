10e0294faf7e0a22566090721b9350a562e232ac
titanium9288 <87443489+titanium9288@users.noreply.github.com>
2025-04-03 17:30:57 +0900

    [Gold IV] Title: 연구소, Time: 1584 ms, Memory: 35036 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,6 +1,8 @@
 from collections import deque
 import sys
+
 sys.setrecursionlimit(10**6)
+
 
 # 경계 체크용 함수
 def in_range(r, c):
@@ -48,8 +50,12 @@ def infection():
                 copy_grid[nr][nc] = 2
                 queue.append((nr, nc))
 
-    # safe = (N * M) - ((sum(map(sum, copy_grid)) - (original_wall_count + 3)) // 2)
-    safe = sum([copy_grid[r][c] == 0 for r in range(N) for c in range(M)])
+    safe = (
+        (N * M)
+        - ((sum(map(sum, copy_grid)) - (original_wall_count + 3)) // 2)
+        - (original_wall_count + 3)
+    )
+    # safe = sum([copy_grid[r][c] == 0 for r in range(N) for c in range(M)])
     return safe
 
 
@@ -59,7 +65,7 @@ grid = [list(map(int, input().split())) for _ in range(N)]
 DIRECTIONS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 VIRUS = [(r, c) for r in range(N) for c in range(M) if grid[r][c] == 2]
 
-# original_wall_count = sum(map(sum, grid)) - (len(VIRUS) * 2)
-original_wall_count = sum([grid[r][c] == 1 for r in range(N) for c in range(M)])
+original_wall_count = sum(map(sum, grid)) - (len(VIRUS) * 2)
+# original_wall_count = sum([grid[r][c] == 1 for r in range(N) for c in range(M)])
 
 print(generate_wall_combinations(0, 0))
