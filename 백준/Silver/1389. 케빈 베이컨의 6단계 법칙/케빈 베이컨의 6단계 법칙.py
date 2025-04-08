94efbb82ce6eac06029796cdb94d256f1682bc09
titanium9288 <87443489+titanium9288@users.noreply.github.com>
2025-04-09 07:40:40 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 60 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -1,28 +1,38 @@
+from collections import deque
 import sys
 
 input = sys.stdin.readline
 
 V, E = map(int, input().split())
-dist = [[0 if u == v else float("inf") for v in range(V + 1)] for u in range(V + 1)]
-users = range(1, V + 1)
+graph = {i: [] for i in range(1, V + 1)}
 
 for _ in range(E):
     u, v = map(int, input().split())
-    dist[u][v] = 1
-    dist[v][u] = 1
-
-# 플로이드-워셜
-for k in users:
-    for u in users:
-        for v in users:
-            dist[u][v] = min(dist[u][k] + dist[k][v], dist[u][v])
+    graph[u].append(v)
+    graph[v].append(u)
 
 # 케빈 베이컨의 수가 가장 작은 사람 구하기
 min_user = 0
 min_KB = float("inf")
 
-for user in users:
-    KB = sum(dist[user][1:])
+# 모든 user에 대해 BFS를 돌려서 케빈 베이컨 수 구하기
+for user in range(1, V + 1):
+    distance = [-1] * (V + 1)
+    distance[user] = 0
+    queue = deque([user])
+
+    # BFS
+    while queue:
+        u = queue.popleft()
+
+        for v in graph[u]:
+            if distance[v] != -1:
+                continue
+
+            distance[v] = distance[u] + 1
+            queue.append(v)
+
+    KB = sum(distance[1:])
     if min_KB > KB:
         min_user = user
         min_KB = KB
