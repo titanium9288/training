c0dd2fb028c31fb487c1f84dee1517c1503940bf
titanium9288 <87443489+titanium9288@users.noreply.github.com>
2025-04-20 18:30:42 +0900

    [Silver I] Title: 절댓값 힙, Time: 132 ms, Memory: 42144 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -0,0 +1,28 @@
+import heapq
+import sys
+
+input = sys.stdin.readline
+
+
+def abs_heap(x):
+    # x가 0일때 heappop
+    if x == 0:
+        if not queue:
+            return 0
+
+        _, val = heapq.heappop(queue)
+        return val
+
+    else:
+        heapq.heappush(queue, (abs(x), x))
+
+
+N = int(input())
+queue = []
+
+for _ in range(N):
+    x = int(input())
+
+    val = abs_heap(x)
+    if val is not None:
+        print(val)
