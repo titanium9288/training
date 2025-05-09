48c7cd847aa5b0a353c6e2a09c0e8b3b48e2114c
titanium9288 <87443489+titanium9288@users.noreply.github.com>
2025-05-09 16:53:57 +0900

    [Silver V] Title: 너의 평점은, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -0,0 +1,27 @@
+T = 20
+
+grade_to_score = {
+    "A+": 4.5,
+    "A0": 4.0,
+    "B+": 3.5,
+    "B0": 3.0,
+    "C+": 2.5,
+    "C0": 2.0,
+    "D+": 1.5,
+    "D0": 1.0,
+    "F": 0.0,
+}
+total_credit = 0
+total_score = 0
+
+for _ in range(T):
+    subject, credit, grade = input().split()
+    credit = float(credit)
+
+    if grade == "P":
+        continue
+
+    total_score += grade_to_score[grade] * credit
+    total_credit += credit
+
+print(total_score / total_credit)
